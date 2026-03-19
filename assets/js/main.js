c5ea3e000e65d222e9d9190401d7285ef884cfd8
Brian Murphy <90574883+bmtechart@users.noreply.github.com>
2026-03-19 11:32:27 -0600

    added image grid for portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,60 @@ navLinks.forEach(link => {
     });
   });
 });
+
+// Open Modal
+
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modalContent");
+const items = document.querySelectorAll(".portfolio-item");
+
+items.forEach(item => {
+	item.addEventListener("click", async () => {
+		const url = item.getAttribute("data-url");
+
+        
+		try {
+			const response = await fetch(url);
+			const html = await response.text();
+
+			modalContent.innerHTML = html;
+			modal.classList.add("show");
+		} catch (err) {
+			modalContent.innerHTML = "<p>Failed to load content.</p>";
+            modal.classList.add("show");
+		}
+            
+	});
+});
+
+// Close Modal
+
+const closeBtn = document.querySelector(".modal-close");
+
+closeBtn.addEventListener("click", () => {
+	modal.classList.remove("show");
+	modalContent.innerHTML = "";
+});
+
+document.addEventListener("click", (e) => {
+	if (e.target.classList.contains("modal-close")) {
+		modal.classList.remove("show");
+		modalContent.innerHTML = "";
+	}
+});
+
+document.addEventListener("keydown", (e) => {
+	if (e.key === "Escape") {
+		modal.classList.remove("show");
+		modalContent.innerHTML = "";
+	}
+});
+
+/*
+modal.addEventListener("click", (e) => {
+	if (e.target === modal) {
+		modal.classList.remove("show");
+		//modalContent.innerHTML = "";
+	}
+});
+*/
